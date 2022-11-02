45ac064e2035d4d82ec42785422ec1bc6e462f3d
Johnny Proano <djimageonline@gmail.com>
2022-11-01 22:01:55 -0400

    readded prodcuts via seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,11 @@
-Product.create!([
-  {name: "chair", price: "98", image_url: "testchair.com", description: "green chair"},
-  {name: "table", price: "200", image_url: "https://images.unsplash.com/photo-1533090481720-856c6e3c1fdc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80", description: "nice white table"}
+product1 = Product.create!([
+  {name: "chair", price: 98, image_url: "testchair.com", description: "green chair"},
+  {name: "table", price: 200, image_url: "https://images.unsplash.com/photo-1533090481720-856c6e3c1fdc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80", description: "nice white table"}
+  {name: "desk", price: 60, image_url: "www.testdesk.com", description: "orange desk"}
+
+
 ])
+
+
+
+
