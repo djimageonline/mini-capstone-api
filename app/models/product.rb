5a41afd0599cb1b81a1178291f5c757343d0362c
Johnny Proano <djimageonline@gmail.com>
2022-11-09 21:27:00 -0500

    rewrote code for model methods - shortcust

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,12 @@
 class Product < ApplicationRecord
-  
+  belongs_to :supplier
+  has_many :images
+
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :price, numericality: {greater_than: 0}
   validates :description, length: {in: 10..500} 
-
-  def images
-    Image.where(product_id: id)
-  end
-
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
 
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
