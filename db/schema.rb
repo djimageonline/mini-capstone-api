51c317dfbeea7ae2dc25634031de81a168f9b774
Johnny Proano <djimageonline@gmail.com>
2022-11-14 20:59:57 -0500

    added name column, seeded new data for category, adjusted requests.http

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_003829) do
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,36 @@
-# product1 = Product.create!(name: "chair", price: 98, image_url: "testchair.com", description: "green chair")
-# product1.save
-
-# product2 = Product.create!(name: "table", price: 200, image_url: "https://images.unsplash.com/photo-1533090481720-856c6e3c1fdc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80", description: "nice white table")
-# product2.save
-
-# product3 = Product.create!(name:"desk", price: 60, image_url: "www.testdesk.com", description: "orange desk")
-# product3.save
-
-
-# supplier1 = Supplier.create!(name: "Fubu", email: "fubu@test.com", phone_number: "777-555-3333")
-# supplier1.save
-
-# supplier2 = Supplier.create!(name: "Nike", email: "nike@test.com", phone_number: "888-444-8888")
-# supplier2.save
-
-# supplier3 = Supplier.create!(name: "Puma", email: "puma@test.com", phone_number: "999-222-1111")
-# supplier3.save
-
-image1 = Image.create!(url: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/2016a636-2953-41b4-b496-55263f2b26bc/air-jordan-1-mid-shoes-M2KS6n.png", product_id: product2.id)
-
-image2 = Image.create!(url: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ccd1396f-f289-4bbf-b385-68dc6e71282b/air-force-1-07-premium-mens-shoes-jTLc6j.png", product_id: product2.id)
+User.create!([
+  {name: "Johnny", email: "Proano", password: "password", admin: false},
+  {name: "Mike", email: "mike@test.com", password: "password", admin: false}
+])
+Supplier.create!([
+  {name: "Fubu", email: "fubu@test.com", phone_number: "777-555-3333"},
+  {name: "Nike", email: "nike@test.com", phone_number: "888-444-8888"},
+  {name: "Puma", email: "puma@test.com", phone_number: "999-222-1111"}
+])
+Product.create!([
+  {name: "table", price: "200.0", description: "nice white table", quantity: nil, supplier_id: 2},
+  {name: "desk", price: "60.0", description: "orange desk", quantity: nil, supplier_id: 3},
+  {name: "chair", price: "98.0", description: "green chair", quantity: nil, supplier_id: 1}
+])
+Order.create!([
+  {user_id: 2, product_id: 2, quantity: 5, subtotal: "100.0", tax: nil, total: "110.0"},
+  {user_id: 1, product_id: 2, quantity: 8, subtotal: "200.0", tax: nil, total: "150.0"},
+  {user_id: 2, product_id: 2, quantity: 10, subtotal: "200.0", tax: nil, total: "150.0"},
+  {user_id: 2, product_id: 3, quantity: 55, subtotal: "250.0", tax: nil, total: "4320.0"},
+  {user_id: 2, product_id: 1, quantity: 10, subtotal: "980.0", tax: "88.2", total: "1068.2"},
+  {user_id: 2, product_id: 1, quantity: 10, subtotal: "980.0", tax: "88.2", total: "1068.2"},
+  {user_id: 2, product_id: 1, quantity: 10, subtotal: "980.0", tax: "88.2", total: "1068.2"},
+  {user_id: 2, product_id: 2, quantity: 44, subtotal: "8800.0", tax: "792.0", total: "9592.0"},
+  {user_id: 2, product_id: 2, quantity: 500, subtotal: "100000.0", tax: "9000.0", total: "109000.0"},
+  {user_id: 2, product_id: 1, quantity: 33, subtotal: "3234.0", tax: "291.06", total: "3525.06"},
+  {user_id: 2, product_id: 1, quantity: 33, subtotal: "3234.0", tax: "291.06", total: "3525.06"},
+  {user_id: 2, product_id: 1, quantity: 33, subtotal: "3234.0", tax: "291.06", total: "3525.06"},
+  {user_id: 2, product_id: 1, quantity: 33, subtotal: "3234.0", tax: "291.06", total: "3525.06"},
+  {user_id: 2, product_id: 1, quantity: 33, subtotal: "3234.0", tax: "291.06", total: "3525.06"},
+  {user_id: 2, product_id: 3, quantity: 222, subtotal: "13320.0", tax: "1198.8", total: "14518.8"},
+  {user_id: 2, product_id: 1, quantity: 555, subtotal: "54390.0", tax: "4895.1", total: "59285.1"}
+])
+Image.create!([
+  {url: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/2016a636-2953-41b4-b496-55263f2b26bc/air-jordan-1-mid-shoes-M2KS6n.png", product_id: 1},
+  {url: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ccd1396f-f289-4bbf-b385-68dc6e71282b/air-force-1-07-premium-mens-shoes-jTLc6j.png", product_id: 3}
+])
